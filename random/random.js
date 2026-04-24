db2ab41ac981b61a1e23ee8bfdbd39c8a5210e2c
vichaiyot <vichaiyotngoensiri@gmail.com>
2026-04-24 17:24:53 +0700

    first commit

---
+++ random/random.js
@@ -0,0 +1,326 @@
+const notSpicy = [{
+    name: "ข้าวไข่เจียว",
+    img: 'https://s359.kapook.com//pagebuilder/9307f48a-a2cf-4cbd-bfad-eecf4b43e763.jpg',
+},
+{
+    name: "ผัดซีอิ๊วหมู",
+    img: 'https://s359.kapook.com//pagebuilder/33d81380-8834-4458-b59b-d37c9f387146.jpg',
+},
+{
+    name: "ผัดซีอิ๊วหมูสับ",
+    img: 'https://s359.kapook.com//pagebuilder/808ba9fb-9671-4419-b33b-0aee884fe678.jpg',
+},
+{
+    name: "ราดหน้า",
+    img: 'https://s359.kapook.com//pagebuilder/7226ffca-9aa3-422f-9c1c-758601ec2dfc.jpg',
+},
+{
+    name: "ผัดมะกะโรนีกุ้ง",
+    img: 'https://s359.kapook.com//pagebuilder/7d5801e5-1724-44f7-87ed-42bf564bac53.jpg',
+},
+{
+    name: "ผัดมะกะโรนีไก่",
+    img: 'https://s359.kapook.com//pagebuilder/4bb250be-bd71-4301-8f4d-268b8510dc46.jpg',
+},
+{
+    name: "ข้าวผัดหมู",
+    img: 'https://s359.kapook.com//pagebuilder/86bade59-2b79-4083-8d57-1399933fcea0.jpg',
+},
+{
+    name: "ข้าวผัดต้มยำ",
+    img: 'https://s359.kapook.com//pagebuilder/b13638e6-6bc2-43cc-a9e0-8181a94799cd.jpg',
+},
+{
+    name: "ข้าวผัดคะน้าปลาเค็ม",
+    img: 'https://s359.kapook.com//pagebuilder/da3901f9-9ce5-4a8b-ae1d-235eacfe4d99.jpg',
+},
+{
+    name: "ข้าวผัดแหนม",
+    img: 'https://s359.kapook.com//pagebuilder/ea1b2e5d-a6bc-4079-8794-2dc43d2d2d46.jpg',
+},
+{
+    name: "ข้าวผัดปลากระป๋อง",
+    img: 'https://s359.kapook.com//pagebuilder/49a68ccd-e18d-4ab0-8b6c-62f781e05a7b.jpg',
+},
+{
+    name: "ข้าวผัดไข่",
+    img: 'https://s359.kapook.com//pagebuilder/b8762faa-9e22-494e-aace-f3480b35e88b.jpg',
+},
+{
+    name: "ผัดไทยกุ้งสด",
+    img: 'https://s359.kapook.com//pagebuilder/e9d03cdd-cf63-4e99-940c-93bbae5db089.jpg',
+},
+{
+    name: "สุกี้แห้ง",
+    img: 'https://s359.kapook.com//pagebuilder/21a0b53c-e55f-4e7d-8303-94d850f31a3b.jpg',
+},
+{
+    name: "สุกี้น้ำ",
+    img: 'https://s359.kapook.com//pagebuilder/1d6b0e4d-38a8-4de3-b773-86ea741cf399.jpg',
+},
+{
+    name: "หมูกระเทียม",
+    img: 'https://s359.kapook.com//pagebuilder/2d57a416-9bd2-4ee3-a8e8-33662187dfb9.jpg',
+},
+{
+    name: "กุ้งกระเทียม",
+    img: 'https://s359.kapook.com//pagebuilder/eff5db1d-dbe3-43e5-b35d-f4edd337e862.jpg',
+},
+{
+    name: "ไก่กระเทียม",
+    img: 'https://s359.kapook.com//pagebuilder/32ea5ba7-607b-48ef-bbf9-e95192c3a93b.jpg',
+},
+{
+    name: "เนื้อทอดกระเทียม",
+    img: 'https://s359.kapook.com//pagebuilder/88ad4aef-8181-4f5f-a346-ff3ddcc9716a.jpg',
+},
+{
+    name: "ผัดผักรวมมิตรกุ้ง",
+    img: 'https://s359.kapook.com//pagebuilder/6072c35a-3d1e-4a0c-8ea8-fca65974f357.jpg',
+},
+{
+    name: "ผัดคะน้าหมูกรอบ",
+    img: 'https://s359.kapook.com//pagebuilder/38948713-a467-497c-9196-630a5513b67c.jpg',
+},
+{
+    name: "ผัดแขนงหมูกรอบ",
+    img: 'https://s359.kapook.com//pagebuilder/e1df13ea-72bd-4f5f-84f1-036f18d35dc5.jpg',
+},
+{
+    name: "ผัดผักบุ้งหมูสับ",
+    img: 'https://s359.kapook.com//pagebuilder/2d70a3bc-8789-4972-a115-e8609c9a4cb0.jpg',
+},
+{
+    name: "กะหล่ำปลีผัดน้ำปลา",
+    img: 'https://s359.kapook.com//pagebuilder/adbc8c61-c00d-4b74-92c3-63017f4f196e.jpg',
+},
+{
+    name: "ปลาหมึกผัดผงกะหรี่",
+    img: 'https://s359.kapook.com//pagebuilder/8afe5b60-1b8f-4c50-8a47-e791a58a5997.jpg',
+},
+{
+    name: "หมูผัดผงกะหรี่",
+    img: 'https://s359.kapook.com//pagebuilder/950b696f-c170-452e-ac02-f778342d40de.jpg',
+},
+{
+    name: "กุ้งผัดผงกะหรี่",
+    img: 'https://s359.kapook.com//pagebuilder/0c4a5dec-4f78-44dc-b6fd-7bd0afc8b203.jpg',
+},
+{
+    name: "ไก่ผัดผงกะหรี่",
+    img: 'https://s359.kapook.com//pagebuilder/63e9cb16-b77d-44e8-9c51-c0175e6c0dd7.jpg',
+},
+{
+    name: "หมูผัดพริกหยวก",
+    img: 'https://s359.kapook.com//pagebuilder/49840cf7-2e67-465d-8a69-b094095e810b.jpg',
+},
+{
+    name: "ไก่ผัดพริกหยวก",
+    img: 'https://s359.kapook.com//pagebuilder/38936ea0-86df-424d-b319-5621be76a6de.jpg',
+},
+{
+    name: "เครื่องในไก่ผัดพริกหยวก",
+    img: 'https://s359.kapook.com//pagebuilder/1bace197-0dc3-47b7-94a3-c48a273a78ff.jpg',
+},
+{
+    name: "แกงจืด",
+    img: 'https://s359.kapook.com//pagebuilder/65237445-4a35-4896-bc66-a19e5ddf0ee7.jpg',
+},
+{
+    name: "พะโล้",
+    img: 'https://i.ytimg.com/vi/QqvRJ1XbUf0/maxresdefault.jpg',
+},
+   
+];
+const spicy = [{
+    name:"ผัดกะเพราหมูสับ",
+    img: 'https://s359.kapook.com//pagebuilder/2f02d228-55ba-429e-9677-80f04b8df91a.jpg',
+},
+{
+    name:"ผัดกะเพรากุ้ง",
+    img: 'https://s359.kapook.com//pagebuilder/22ca18ee-782a-46b6-8ae0-568fb335d48e.jpg',
+},
+{
+    name:"กะเพราเนื้อไข่ดาว",
+    img: 'https://s359.kapook.com//pagebuilder/5c8b12df-3e14-459c-b6ec-619b101a504d.jpg',
+},
+{
+    name:"ผัดกะเพราหมูกรอบ",
+    img: 'https://s359.kapook.com//pagebuilder/d022ab92-b7e7-49df-8bbf-db99b2d889b3.jpg',
+},
+{
+    name:"ผัดกะเพราไก่",
+    img: 'https://s359.kapook.com//pagebuilder/5f0bce21-6748-4143-ab94-afdec6eb258a.jpg',
+},
+{
+    name:"ผัดกะเพราปลาหมึก",
+    img: 'https://s359.kapook.com//pagebuilder/23cb9777-6432-4a44-85bb-6c62a0a6a1bf.jpg',
+},
+{
+    name:"ผัดกะเพราไข่เยี่ยวม้าหมูสับ",
+    img: 'https://s359.kapook.com//pagebuilder/0b2fb973-ec01-4d54-9479-d62b20c079d6.jpg',
+},
+{
+    name:"ผัดกะเพราทะเล",
+    img: 'https://s359.kapook.com//pagebuilder/2e1104cd-bb21-4cee-8cf8-07fc851f5922.jpg',
+},
+{
+    name:"ผัดกะเพรากุนเชียง",
+    img: 'https://s359.kapook.com//pagebuilder/fa8fae73-3414-4f7b-9960-a45277ba4901.jpg',
+},
+{
+    name:   "ผัดกะเพราหมูยอ",
+    img: 'https://s359.kapook.com//pagebuilder/8ed458a9-f70e-44e3-916b-5699d3af6ffa.jpg',
+},
+{
+    name: "ผัดขี้เมาหมู",
+    img: 'https://s359.kapook.com//pagebuilder/9a3a9240-21b9-4400-88cc-d1c50a7b8dbf.jpg',
+},
+{
+    name:"ผัดขี้เมาไก่",
+    img: 'https://s359.kapook.com//pagebuilder/6c38e474-5523-41fb-b357-70a197d54241.jpg',
+},
+{
+    name:"ผัดขี้เมาทะเล",
+    img: 'https://s359.kapook.com//pagebuilder/5a7fd193-4ef6-40d1-9b3d-17958c326360.jpg',
+},
+{
+    name:"ผัดขี้เมาเนื้อ",
+    img: 'https://s359.kapook.com//pagebuilder/1a30253d-8da7-430d-ab5f-89d45b5e5f1b.jpg',
+},
+{
+    name:"มาม่าผัดขี้เมาหมูกรอบ",
+    img: 'https://s359.kapook.com//pagebuilder/31965a4c-8432-454b-bd8f-58c29d374c1c.jpg',
+},
+{
+    name:"มาม่าผัดขี้เมาทะเล",
+    img: 'https://s359.kapook.com//pagebuilder/498c0b65-e08b-4a82-8a64-c0d9b60c28ab.jpg',
+},
+{
+    name:"มาม่าผัดขี้เมาหมู",
+    img: 'https://s359.kapook.com//pagebuilder/63de309b-c473-4ee0-8923-ea171fdb3c5f.jpg',
+},
+{
+    name:"ผัดพริกแกงไก่",
+    img: 'https://s359.kapook.com//pagebuilder/b0340fbf-e599-4f4b-a57c-ff946ef02ebb.jpg',
+},
+{
+    name:"ผัดพริกแกงเนื้อ",
+    img: 'https://s359.kapook.com//pagebuilder/17ccfae0-c434-41fa-89f9-69ed67bdbb0c.jpg',
+},
+{
+    name:"ผัดพริกแกงทะเล",
+    img: 'https://s359.kapook.com//pagebuilder/c13a1983-49ac-4572-b3b8-3d38bfafe01c.jpg',
+},
+{
+    name:"ผัดพริกแกงหมู",
+    img: 'https://s359.kapook.com//pagebuilder/5ea0d28c-7b8b-4309-a715-cb98157d1ce5.jpg',
+},
+{
+    name:"หมูผัดพริกเผา",
+    img: 'https://s359.kapook.com//pagebuilder/b4128d68-f52e-446e-9da5-8bd72fb6bd4c.jpg',
+},
+{
+    name:"ปลาหมึกผัดพริกเผา",
+    img: 'https://s359.kapook.com//pagebuilder/1145b254-0ea2-45e4-859b-b64a82f69be5.jpg',
+},
+{
+    name:"ยำวุ้นเส้น",
+    img: 'https://s359.kapook.com//pagebuilder/1cd1c77a-add6-40c1-bf51-ffed423d148b.jpg',
+},
+{
+    name:"ยำมาม่า",
+    img: 'https://s359.kapook.com//pagebuilder/77060d2d-0d54-4453-b4b4-07a9ab20dd7a.jpg',
+},
+{
+    name:"ยำปลากระป๋อง",
+    img: 'https://s359.kapook.com//pagebuilder/db0f8d6f-e8d6-4c34-ab66-1e43aafba853.jpg',
+},
+{
+    name: "ต้มยำกุ้ง",
+    img: 'https://s359.kapook.com//pagebuilder/a85372d8-f511-4b02-92bc-886199a41232.jpg',
+},
+{
+    name: "ผัดพริกแกงปลาหมึก",
+    img: 'https://i.ytimg.com/vi/hVgCuTqDXx4/maxresdefault.jpg',
+},
+{
+    name: "กะเพราเต้าหู๋๋ไข่",
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcOUTWlt8eFaMMAv3FtlRwOx-FYLSXda7ynw&s',
+}, ];
+
+
+var modla = document.getElementById('02id');
+function handlaButtonClick(){
+    var selectElement = document.getElementById('Select');
+    var selectedVaule = selectElement.value;
+    modla.style.display ='block';
+
+    var html = '';
+    if(selectedVaule === 'spicy'){
+        var spicyrandom = Math.floor(Math.random() * spicy.length);
+       
+        html += `
+        <div onclick="Close()" class="modla-bg">
+            <div class="modla-box">
+                <div class="modla-con">
+                    <a onclick="Close()" class="close" >&times;</a>
+                    <h4>เมนูที่ได้</h4>
+                    
+                    <div class="img-item">
+                    <img src="${spicy[spicyrandom].img}" alt="" >
+                    </div>
+                    <h2 id="01id">${spicy[spicyrandom].name}</h2>
+                </div>
+            </div>
+        </div>
+    `
+    $("#02id").html(html);
+    }
+    else if(selectedVaule === 'notSpicy'){
+        var randomnospicy = Math.floor(Math.random() * notSpicy.length);
+        html += `
+        <div onclick="Close()" class="modla-bg">
+            <div class="modla-box">
+                <div class="modla-con">
+                    <a onclick="Close()" class="close" >&times;</a>
+                    <h4>เมนูที่ได้</h4>
+                    
+                    <div class="img-item">
+                    <img src="${notSpicy[randomnospicy].img}" alt="" >
+                   </div>
+                    <h2 id="01id">${notSpicy[randomnospicy].name}</h2>
+                </div>
+            </div>
+        </div>
+    `
+    $("#02id").html(html);
+       
+    }
+    else{
+            Swal.fire({
+                icon: 'warning',
+                title: 'กรุณาเลือกก่อนนะครับผม!'
+            })
+            modla.style.display ='none';
+    }
+}
+
+function Close(){
+    modla.style.display ='none';
+    }
+
+ function clickDd(){
+    document.getElementById('Dd').style.display = 'block';
+ } 
+ 
+ window.onclick = function(event){
+    if(!event.target.matches('.bar')){
+        var dropdowns = document.getElementsByClassName("drop");
+        for(var i=0; i<dropdowns.length; i++){
+            if(document.getElementById('Dd').style.display = 'block'){
+                document.getElementById('Dd').style.display = 'none';
+            }
+        }
+    }
+ }
